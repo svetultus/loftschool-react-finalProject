e91935a102a97f1730f60bb4446d04f03f78b6e8
svetultus <svetultus@gmail.com>
2019-08-30 11:15:20 +0300

    forms with react final form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,13 @@
 import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import { withRouter, Redirect } from "react-router-dom";
+import { Form, Field } from "react-final-form";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import styles from "./Login.module.css";
 import { getIsAuthorized, authRequest } from "../../modules/Auth";
 import { getUserData } from "../../modules/User";
-import { connect } from "react-redux";
-import { withRouter, Redirect } from "react-router-dom";
 import { user } from "./authData";
-
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
 
 const MapStateToProps = state => ({
   isAuthorized: getIsAuthorized(state),
@@ -36,12 +36,13 @@ class Login extends PureComponent {
     }
   };
   onSubmit = event => {
-    event.preventDefault();
+    // event.preventDefault();
+    console.log(event);
     const { authRequest } = this.props;
-    const { userName, userPassword } = event.target;
+    const { userName, userPassword } = event;
     authRequest({
-      userName: userName["value"],
-      userPassword: userPassword["value"]
+      userName: userName,
+      userPassword: userPassword
     });
   };
 
@@ -56,30 +57,71 @@ class Login extends PureComponent {
     return (
       <div className={styles.root}>
         <h1>Войти</h1>
-        <form onSubmit={this.onSubmit}>
-          <TextField
-            name="userName"
-            ref={this.input}
-            placeholder="Имя"
-            label="Имя"
-            type="text"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-          <TextField
-            name="userPassword"
-            ref={this.input}
-            placeholder="Пароль"
-            label="Пароль"
-            type="password"
-            autoComplete="current-password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <Button variant="contained" type="submit">
-            Войти
-          </Button>
-        </form>
+        <Form
+          onSubmit={this.onSubmit}
+          validate={this.validate}
+          initialValues={{
+            userName: this.state.name,
+            userPassword: this.state.password
+          }}
+          render={({
+            handleSubmit,
+            reset,
+            submitting,
+            pristine,
+            values,
+            errors
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <Field name="userName" onChange={this.handleChange}>
+                {({ input, meta }) => {
+                  return (
+                    <React.Fragment>
+                      <TextField
+                        {...input}
+                        ref={this.input}
+                        placeholder="Логин"
+                        label="Логин"
+                        type="text"
+                        required={true}
+                        name={input.name}
+                        value={input.value}
+                        onChange={input.onChange}
+                      />
+                      <div className="error">{meta.touched && meta.error}</div>
+                    </React.Fragment>
+                  );
+                }}
+              </Field>
+              <Field name="userPassword" onChange={this.handleChange}>
+                {({ input, meta }) => {
+                  return (
+                    <React.Fragment>
+                      <TextField
+                        {...input}
+                        name="userPassword"
+                        ref={this.input}
+                        placeholder="Пароль"
+                        label="Пароль"
+                        type="password"
+                        required={true}
+                        autoComplete="current-password"
+                        name={input.name}
+                        value={input.value}
+                        onChange={input.onChange}
+                      />
+                      <div className="error">{meta.touched && meta.error}</div>
+                    </React.Fragment>
+                  );
+                }}
+              </Field>
+
+              <Button type="submit" disabled={submitting}>
+                Сохранить
+              </Button>
+            </form>
+          )}
+        />
       </div>
     );
   }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,16 +1,17 @@
 import React, { PureComponent } from "react";
-import styles from "./Profile.module.css";
+import { render } from "react-dom";
+import { Link, withRouter } from "react-router-dom";
 import cx from "classnames";
-import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { Form, Field } from "react-final-form";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import {
   userSuccess,
   checkUserIsPayable,
   getUserData
 } from "../../modules/User";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+import styles from "./Profile.module.css";
 
 const MapStateToProps = state => ({
   userProfile: getUserData(state)
@@ -48,62 +49,163 @@ class Profile extends PureComponent {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
+
+  validate = values => {
+    const errors = {};
+    console.log("validate");
+    if (!values.userName) {
+      errors.userName = "Имя пользователя должно быть заполнено";
+    }
+    if (!values.cardName) {
+      errors.cardName = "Название карты должно быть заполнено";
+    }
+    if (!values.cardNumber) {
+      errors.cardNumber = "Номер карты должен быть заполнен";
+    }
+    if (!values.expDate) {
+      errors.expDate = "Срок действия карты должен быть заполнен";
+    }
+    if (!values.cvv) {
+      errors.cvv = "CVV должно быть заполнено";
+    }
+    return errors;
+  };
   render() {
     return (
       <React.Fragment>
         <h1>Профиль</h1>
         <h2>Способ оплаты</h2>
-
-        <form onSubmit={this.onSubmit}>
-          <TextField
-            name="userName"
-            placeholder="Имя владельца"
-            label="Имя владельца"
-            type="text"
-            required={true}
-            value={this.state.userName}
-            onChange={this.handleChange}
-          />
-          <TextField
-            name="cardName"
-            placeholder="Вид карты"
-            label="Вид карты"
-            type="text"
-            required={true}
-            value={this.state.cardName}
-            onChange={this.handleChange}
-          />
-          <TextField
-            name="cardNumber"
-            placeholder="Номер карты"
-            label="Номер карты"
-            type="text"
-            required={true}
-            value={this.state.cardNumber}
-            onChange={this.handleChange}
-          />
-          <TextField
-            name="expDate"
-            placeholder="Дата окончания действия"
-            label="Дата окончания действия"
-            type="date"
-            required={true}
-            value={this.state.expDate}
-            onChange={this.handleChange}
-          />
-          <TextField
-            name="cvv"
-            placeholder="CVV"
-            label="CVV"
-            type="text"
-            required={true}
-            value={this.state.cvv}
-            onChange={this.handleChange}
-          />
-          <Button variant="contained" type="submit">
-            Сохранить
-          </Button>
-        </form>
+        <Form
+          onSubmit={this.onSubmit}
+          validate={this.validate}
+          initialValues={{
+            userName: "",
+            cardName: "",
+            cardNumber: "",
+            expDate: "",
+            cvv: ""
+          }}
+          render={({
+            handleSubmit,
+            reset,
+            submitting,
+            pristine,
+            values,
+            errors
+          }) => {
+            return (
+              <form onSubmit={handleSubmit}>
+                <Field name="userName">
+                  {({ input, meta }) => {
+                    return (
+                      <React.Fragment>
+                        <TextField
+                          {...input}
+                          placeholder="Имя владельца"
+                          label="Имя владельца"
+                          type="text"
+                          required={true}
+                          name={input.name}
+                          value={input.value}
+                          onChange={input.onChange}
+                        />
+                        {meta.error && meta.touched && (
+                          <div className="error">{meta.error}</div>
+                        )}
+                      </React.Fragment>
+                    );
+                  }}
+                </Field>
+                <Field name="cardName">
+                  {({ input, meta }) => {
+                    return (
+                      <React.Fragment>
+                        <TextField
+                          {...input}
+                          placeholder="Вид карты"
+                          label="Вид карты"
+                          type="text"
+                          required={true}
+                          name={input.name}
+                          value={input.value}
+                          onChange={input.onChange}
+                        />
+                        {meta.error && meta.touched && (
+                          <div className="error">{meta.error}</div>
+                        )}
+                      </React.Fragment>
+                    );
+                  }}
+                </Field>
+                <Field name="cardNumber">
+                  {({ input, meta }) => {
+                    return (
+                      <React.Fragment>
+                        <TextField
+                          {...input}
+                          placeholder="Номер карты"
+                          label="Номер карты"
+                          type="text"
+                          required={true}
+                          name={input.name}
+                          value={input.value}
+                          onChange={input.onChange}
+                        />
+                        {meta.error && meta.touched && (
+                          <div className="error">{meta.error}</div>
+                        )}
+                      </React.Fragment>
+                    );
+                  }}
+                </Field>
+                <Field name="expDate">
+                  {({ input, meta }) => {
+                    return (
+                      <React.Fragment>
+                        <TextField
+                          {...input}
+                          label="Дата окончания действия"
+                          type="date"
+                          required={true}
+                          name={input.name}
+                          value={input.value}
+                          onChange={input.onChange}
+                        />
+                        {meta.error && meta.touched && (
+                          <div className="error">{meta.error}</div>
+                        )}
+                      </React.Fragment>
+                    );
+                  }}
+                </Field>
+                <Field name="cvv">
+                  {({ input, meta }) => {
+                    return (
+                      <React.Fragment>
+                        <TextField
+                          {...input}
+                          placeholder="CVV"
+                          label="CVV"
+                          type="text"
+                          required={true}
+                          name={input.name}
+                          value={input.value}
+                          onChange={input.onChange}
+                        />
+                        {meta.error && meta.touched && (
+                          <div className="error">{meta.error}</div>
+                        )}
+                      </React.Fragment>
+                    );
+                  }}
+                </Field>
+                <Button type="submit" disabled={submitting || pristine}>
+                  Сохранить
+                </Button>
+              </form>
+            );
+          }}
+        />
       </React.Fragment>
     );
   }
